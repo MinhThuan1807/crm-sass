3e0e02a0a5ce5a1722102dc346015556fb77ff8f
MinhThuan1807 <coderthuanno1@gmail.com>
2026-03-16 16:54:29 +0700

    chore: fix embedded repo, add prisma schema

---
+++ be/src/common/config.ts
@@ -0,0 +1,48 @@
+import z from 'zod';
+
+import fs from 'fs';
+import path from 'path';
+import { config } from 'dotenv';
+
+// Load environment variables from .env file
+config({
+  path: '.env',
+});
+
+// Check if .env file exists
+if (!fs.existsSync(path.resolve('.env'))) {
+  console.log('Not found .env file!');
+  process.exit(1);
+}
+
+const ConfigSchema = z.object({
+  DATABASE_URL: z.string(),
+  // ACCESS_TOKEN_SECRET: z.string(),
+  // ACCESS_TOKEN_EXPIRES_IN: z.string(),
+  // REFRESH_TOKEN_SECRET: z.string(),
+  // REFRESH_TOKEN_EXPIRES_IN: z.string(),
+  // SECRET_API_KEY: z.string(),
+  // ADMIN_NAME: z.string(),
+  // ADMIN_PASSWORD: z.string(),
+  // ADMIN_EMAIL: z.string(),
+  // ADMIN_PHONE: z.string(),
+  // OTP_EXPIRES_IN: z.string(),
+  // RESEND_API_KEY: z.string(),
+});
+
+// const configServer = plainToInstance(ConfigSchema, process.env);
+const configServer = ConfigSchema.safeParse(process.env);
+
+if (!configServer.success) {
+  console.log('Invalid environment variables:');
+  // throw configServer.error;
+  console.log(configServer.error);
+  process.exit(1);
+}
+
+// console.log(process.env)
+// console.log(e);
+
+const envConfig = configServer.data;
+
+export default envConfig;
