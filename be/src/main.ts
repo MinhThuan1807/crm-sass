e337eb88f4018d2c92081370f9e856d24b939283
MinhThuan1807 <coderthuanno1@gmail.com>
2026-03-25 14:37:25 +0700

    fix: update server start command to specify port and listen on all interfaces

--- be/src/main.ts
+++ be/src/main.ts
@@ -13,7 +13,7 @@ async function bootstrap() {
     credentials: true,
   });
   const port = envConfig.PORT  || 3001;
-  await app.listen(port);
+  await app.listen(port, "0.0.0.0");
   console.log("Server running on port:", port);
 }
 bootstrap();
